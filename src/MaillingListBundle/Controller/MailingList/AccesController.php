00771464990710a5e565cd66760357afc8a3f5df
julien <guenillon@gmail.com>
2017-05-02 16:53:41 +0200

    JPIE : Insight 4
    A route should always have a valid HTTP method
    Default session cookie's name should be changed.
    Symfony secret should be changed 
    Twig templates should not have syntax errors

--- src/MaillingListBundle/Controller/MailingList/AccesController.php
+++ src/MaillingListBundle/Controller/MailingList/AccesController.php
@@ -4,11 +4,13 @@ namespace MaillingListBundle\Controller\MailingList;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class AccesController extends Controller
 {
     /**
      * @Route("/", name="home")
+     * @Method("GET")
      */
     public function indexAction()
     {
